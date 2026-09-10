99b3fe10ad3020959703dc20fd7ca0ddbe9dee14
Filip <filip.ivanovski@inellipse.com>
2026-09-10 04:55:26 +0200

    Changing IP address in network security configuration and base url for fcm api

--- app/src/main/java/com/example/localinformant/core/data/constants/FirebaseApiConstants.kt
+++ app/src/main/java/com/example/localinformant/core/data/constants/FirebaseApiConstants.kt
@@ -2,5 +2,6 @@ package com.example.localinformant.core.data.constants
 
 object FirebaseApiConstants {
 
-    const val BASE_URL = "http://192.168.0.17:8080/"
+//    const val BASE_URL = "http://192.168.0.17:8080/"
+    const val BASE_URL = "http://10.117.32.50:8080/"
 }
